ac07da39768a02436e8957a413e60b6f76b5196e
Dominik <dominik.otto@gmail.com>
2014-01-28 22:46:22 +0000

    send ip to a file on a server

---
+++ sendip.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+oldip=''
+touch myips
+
+while true; do
+    newip=`lynx --dump http://ipecho.net/plain | grep .`
+    if [[ $newip != $oldip ]]; then
+        echo `date`":$newip" >> myips
+        rsync myips mi:~/piip
+        echo `date`" sendet new IP: $newip"
+        oldip=$newip
+    fi
+    sleep 5m
+done
