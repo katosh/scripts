17c49541871f147f2a6286aa142cfd96dd90e6bb
Dominik <dominik.otto@gmail.com>
2014-01-28 23:17:47 +0000

    spelling correction

--- sendip.sh
+++ sendip.sh
@@ -8,7 +8,7 @@ while true; do
     if [[ $newip != $oldip ]]; then
         echo `date`":$newip" >> myips
         rsync -z myips mi:~/piip.txt
-        echo `date`" sendet new IP: $newip"
+        echo `date`" sended new IP: $newip"
         oldip=$newip
     fi
     sleep 5m
