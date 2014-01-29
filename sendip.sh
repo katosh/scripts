0ec8751a49f40909477873eb4538cfa9c6ca47e8
Dominik <dominik.otto@gmail.com>
2014-01-29 00:29:22 +0000

    added server

--- sendip.sh
+++ sendip.sh
@@ -8,6 +8,7 @@ while true; do
     if [[ $newip != $oldip ]]; then
         echo `date`":$newip" >> myips
         rsync -z myips mi:~/piip.txt
+        rsync -z myips zedat:~/public_html/piip.txt
         echo `date`" sended new IP: $newip"
         oldip=$newip
     fi
