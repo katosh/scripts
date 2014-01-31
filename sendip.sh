c0e8fea94a5bd41f5c251b91e6c92367481d35e5
Dominik <dominik.otto@gmail.com>
2014-01-31 12:34:46 +0000

    just last ip is public now

--- sendip.sh
+++ sendip.sh
@@ -2,13 +2,16 @@
 
 oldip=''
 touch myips
+touch mylastip
 
 while true; do
     newip=`lynx --dump http://ipecho.net/plain | xargs`
     if [[ $newip != $oldip ]]; then
         echo `date`": $newip" >> myips
+        echo $newip > mylastip
         rsync -z myips mi:~/piip.txt
-        rsync -z myips zedat:~/public_html/piip.txt
+        rsync -z myips zedat:~/piip.txt
+        rsync -z mylastip zedat:~/public_html/piip.txt
         echo `date`" sended new IP: $newip"
         oldip=$newip
     fi
