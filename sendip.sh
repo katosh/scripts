3ada8c052074df78aa0f3cb419577b9bfa3d3a47
Dominik <dominik.otto@gmail.com>
2014-02-13 01:43:35 +0000

    only publish when ip is fetched

--- sendip.sh
+++ sendip.sh
@@ -8,12 +8,15 @@ while true; do
     newip=`lynx --dump http://ipecho.net/plain | xargs`
     if [[ $newip != $oldip ]]; then
         echo `date`": $newip" >> myips
-        echo $newip > mylastip
         rsync -z myips mi:~/piip.txt
         rsync -z myips zedat:~/piip.txt
-        rsync -z mylastip zedat:~/public_html/piip.txt
-        echo `date`" sended new IP: $newip"
+        # when it really is an IP
+        if echo $newip | grep '^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$' &>/dev/null; then
+            echo $newip > mylastip
+            rsync -z mylastip zedat:~/public_html/piip.txt
+        fi
+        #echo `date`" sended new IP: $newip"
         oldip=$newip
     fi
-    sleep 5m
+    sleep 10m
 done
